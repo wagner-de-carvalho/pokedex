ffbd8e47cf41ac32c178edddf11b5a2320b1f9c6
wagner <wagncarv@outlook.com>
2023-07-31 11:42:13 -0300

    pokeapi service, all pkemons

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +14,16 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap((res) => res),
       tap((res) => {
-        console.log(res);
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemon(resPokemons.url).subscribe(
+            (res) => (resPokemons.status = res)
+          );
+        });
       })
     );
+  }
+
+  public apiGetPokemon(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res: any) => res));
   }
 }
