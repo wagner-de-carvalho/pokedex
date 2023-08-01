b40b51342e829c3cb77fb0e8441a59b33393b61f
wagner <wagncarv@outlook.com>
2023-08-01 17:13:38 -0300

    componente details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+
+  get pokemon() {
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.url}/${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
