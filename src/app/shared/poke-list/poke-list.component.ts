fc44ac46e52326aed57a8d56da0f666e7cf42d6f
wagner <wagncarv@outlook.com>
2023-08-01 23:41:14 -0300

    componente pokelist, animation

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,14 +9,20 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 export class PokeListComponent implements OnInit {
   private setAllPokemons: any;
   public getAllPokemons: any;
+  public apiError: boolean = false;
 
   constructor(private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemons.subscribe((res) => {
-      this.setAllPokemons = res.results;
-      this.getAllPokemons = this.setAllPokemons;
-    });
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      (error) => {
+        this.apiError = true;
+      }
+    );
   }
 
   public getSearch(value: string) {
